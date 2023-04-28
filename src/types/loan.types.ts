e5166149077ca50a614998d8f69bdd05396cfae9
Martin Flisar <martinflisar@gmail.com>
2023-04-28 20:03:13 +0200

    Added some NFT collections to 'Loan_Collections'

--- src/types/loan.types.ts
+++ src/types/loan.types.ts
@@ -30,8 +30,11 @@ interface Loan_PostBody {
 // Objects
 //==================================================================================================
 const Loan_Collections = {
+    azuki: "0xED5AF388653567Af2F388E6224dC7C4b3241C544",
     boredapeyachtclub: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
     mutantapeyachtclub: "0x60E4d786628Fea6478F785A6d7e704777c86a7c6",
+    moonbirds: "0x23581767a106ae21c074b2276D25e5C3e136a68b",
+    meebits: "0x7Bd29408f11D2bFC23c34f18275bBf23bB716Bc7"
     // TODO
 }
 
